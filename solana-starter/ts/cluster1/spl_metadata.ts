fffb7585886568b74a48b417061c4981afbe9f78
kimerran <mh.neri@gmail.com>
2024-02-27 06:01:26 +0800

    add spl_metadata

--- solana-starter/ts/cluster1/spl_metadata.ts
+++ solana-starter/ts/cluster1/spl_metadata.ts
@@ -1,15 +1,18 @@
 import wallet from "../wba-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { 
-    createMetadataAccountV3, 
-    CreateMetadataAccountV3InstructionAccounts, 
+import {
+    createMetadataAccountV3,
+    CreateMetadataAccountV3InstructionAccounts,
     CreateMetadataAccountV3InstructionArgs,
     DataV2Args
 } from "@metaplex-foundation/mpl-token-metadata";
 import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
+import { PublicKey } from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const MINT_ADDRESS = "5PfvFTM6Jpy4wbGh95HxEaBjqDRjCpTb9Wd47tVfTMsr";
+const mint = publicKey(MINT_ADDRESS);
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -19,30 +22,58 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 
 (async () => {
     try {
+        const metadataProgramId = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s')
+        const metadataSeeds = [
+            Buffer.from('metadata'),
+            metadataProgramId.toBuffer(),
+            new PublicKey(MINT_ADDRESS).toBuffer()
+        ];
+        const [pda] = PublicKey.findProgramAddressSync(metadataSeeds, metadataProgramId)
+        const pdaUmi = publicKey(pda.toString());
+
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            metadata: pdaUmi,
+            mint,
+            mintAuthority: signer,
+        }
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name: 'Baliw ka UMI',
+            symbol: 'BAKAUMI',
+            uri: 'https://arweave.net/judvAOVJdBnulCCMH-ImLDrUl8DIziEwiONJ-J4Z6M0',
+            sellerFeeBasisPoints: 10000,
+            creators: [
+                {
+                    address: keypair.publicKey,
+                    verified: true,
+                    share: 100
+                }
+            ],
+            collection: null,
+            uses: null,
+        }
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            data,
+            isMutable: true,
+            collectionDetails: {
+                size: 1,
+                __kind: 'V1'
+            }
+        }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        )
 
-        // let result = await tx.sendAndConfirm(umi).then(r => r.signature.toString());
-        // console.log(result);
-    } catch(e) {
+        let result = await tx.sendAndConfirm(umi).then(r => r.signature.toString());
+        console.log(result);
+    } catch (e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
 })();
